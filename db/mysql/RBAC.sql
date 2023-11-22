47390b343bd69b1031e6ad29f07e4617e8430b7a
masterE <master___e@163.com>
2023-11-22 22:52:54 +0800

    task - 0011 - 权限系统表设计

---
+++ db/mysql/RBAC.sql
@@ -0,0 +1,214 @@
+DROP TABLE IF EXISTS `rbac_user`;
+CREATE TABLE `rbac_user`
+(
+    `user_id_`   varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '主键ID',
+    `user_no_`   varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户账号',
+    `user_name_` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户名称',
+    `phone_`     varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '手机号码',
+    `email_`     varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '电子邮箱',
+    PRIMARY KEY (`user_id_`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT = '用户'
+  ROW_FORMAT = Dynamic;
+
+
+DROP TABLE IF EXISTS `rbac_auth`;
+CREATE TABLE `rbac_auth`
+(
+    `auth_id_`   varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '权限id',
+    `auth_name_` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '权限名称',
+    `auth_type_` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '权限类型',
+    `parent_id_` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '父级权限id',
+    PRIMARY KEY (`auth_id_`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT = '权限'
+  ROW_FORMAT = Dynamic;
+
+
+
+DROP TABLE IF EXISTS `rbac_role`;
+CREATE TABLE `rbac_role`
+(
+    `role_id_`   varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色id',
+    `role_name_` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '角色名称',
+    `parent_id_` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '父级角色id',
+    PRIMARY KEY (`role_id_`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT = '角色'
+  ROW_FORMAT = Dynamic;
+
+
+DROP TABLE IF EXISTS `rbac_user_role`;
+CREATE TABLE `rbac_user_role`
+(
+    `id_`       varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'id',
+    `user_id_` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户id',
+    `role_id_` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色id',
+    PRIMARY KEY (`id_`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT = '用户-角色 关联表'
+  ROW_FORMAT = Dynamic;
+
+
+DROP TABLE IF EXISTS `rbac_role_auth`;
+CREATE TABLE `rbac_role_auth`
+(
+    `id_`       varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'id',
+    `role_id_` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色id',
+    `auth_id_` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '权限id',
+    PRIMARY KEY (`id_`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT = '角色-权限 关联表'
+  ROW_FORMAT = Dynamic;
+
+
+
+DROP TABLE IF EXISTS `rbac_union`;
+CREATE TABLE `rbac_union`
+(
+    `union_id_`   varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '组织id',
+    `union_name_` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '组织名称',
+    `parent_id_`  varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '父级组织id',
+    PRIMARY KEY (`union_id_`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT = '组织'
+  ROW_FORMAT = Dynamic;
+
+
+DROP TABLE IF EXISTS `rbac_position`;
+CREATE TABLE `rbac_position`
+(
+    `position_id_`   varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '职位id',
+    `position_name_` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '职位名称',
+    `parent_id_`     varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '父级职位id',
+    PRIMARY KEY (`position_id_`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT = '职位'
+  ROW_FORMAT = Dynamic;
+
+
+DROP TABLE IF EXISTS `rbac_team`;
+CREATE TABLE `rbac_team`
+(
+    `team_id_`   varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '团队id',
+    `team_name_` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '团队名称',
+    PRIMARY KEY (`team_id_`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT = '团队'
+  ROW_FORMAT = Dynamic;
+
+
+DROP TABLE IF EXISTS `rbac_user_union`;
+CREATE TABLE `rbac_user_union`
+(
+    `id_`        varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'id',
+    `user_id_`  varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户id',
+    `union_id_` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '组织id',
+    PRIMARY KEY (`id_`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT = '用户-组织 关联表'
+  ROW_FORMAT = Dynamic;
+
+
+
+DROP TABLE IF EXISTS `rbac_user_position`;
+CREATE TABLE `rbac_user_position`
+(
+    `id_`           varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'id',
+    `user_id_`     varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户id',
+    `position_id_` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '职位id',
+    PRIMARY KEY (`id_`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT = '用户-职位 关联表'
+  ROW_FORMAT = Dynamic;
+
+
+
+DROP TABLE IF EXISTS `rbac_user_team`;
+CREATE TABLE `rbac_user_team`
+(
+    `id_`       varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'id',
+    `user_id_` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户id',
+    `team_id_` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '团队id',
+    PRIMARY KEY (`id_`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT = '用户-团队 关联表'
+  ROW_FORMAT = Dynamic;
+
+
+
+DROP TABLE IF EXISTS `rbac_union_role`;
+CREATE TABLE `rbac_union_role`
+(
+    `id_`        varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'id',
+    `union_id_` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '组织id',
+    `role_id_`  varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色id',
+    PRIMARY KEY (`id_`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT = '组织-角色 关联表'
+  ROW_FORMAT = Dynamic;
+
+
+
+DROP TABLE IF EXISTS `rbac_position_role`;
+CREATE TABLE `rbac_position_role`
+(
+    `id_`           varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'id',
+    `position_id_` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '职位id',
+    `role_id_`     varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色id',
+    PRIMARY KEY (`id_`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT = '职位-角色 关联表'
+  ROW_FORMAT = Dynamic;
+
+
+DROP TABLE IF EXISTS `rbac_team_role`;
+CREATE TABLE `rbac_team_role`
+(
+    `id_`       varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'id',
+    `team_id_` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '团队id',
+    `role_id_` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色id',
+    PRIMARY KEY (`id_`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT = '团队-角色 关联表'
+  ROW_FORMAT = Dynamic;
+
+
+DROP TABLE IF EXISTS `rbac_auth_exclusion`;
+CREATE TABLE `rbac_auth_exclusion`
+(
+    `auth_id_`       varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '权限id',
+    `auth_id_obj_`   varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '互斥权限id',
+    `exclusion_desc_` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '权限互斥说明',
+    PRIMARY KEY (`auth_id_`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT = '权限互斥'
+  ROW_FORMAT = Dynamic;
+
+
+
+
+
+
+
+
+
+
+
+
+
